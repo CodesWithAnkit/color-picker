56fd1faa654d69b572155f095cb5ac5dbfb3fefb
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-01-11 18:37:27 +0530

    Create a react App and Added Pallet Colors

---
+++ src/Pallet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pallet = () => {
+  return (
+    <div className="Palette">
+      <div className="Palette-colors"></div>
+    </div>
+  );
+};
+
+export default Pallet;
