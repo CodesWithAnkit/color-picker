46c7d8e0ed72bf9bb7b71285eca72f24d325c185
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-01-12 15:39:10 +0530

    Add colorHelper for generate color shades

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Pallet from './Pallet';
 import ColorPaller from './seedColors';
+import { generatePallet } from './ColorHelpers';
 import './App.css';
 
 function App() {
+  console.log(generatePallet(ColorPaller[2]));
+
   return (
     <div className="App">
       <Pallet {...ColorPaller[2]} />
---
+++ src/ColorHelpers.js
@@ -0,0 +1,50 @@
+import chroma from 'chroma-js';
+const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+function generatePallet(starterPallet) {
+  let newPallet = {
+    palletName: starterPallet.palletName,
+    id: starterPallet.id,
+    emoji: starterPallet.emoji,
+    colors: {}
+  };
+  for (let level of levels) {
+    newPallet.colors[level] = [];
+  }
+  for (let color of starterPallet.colors) {
+    let scale = generateScale(color.color, 10).reverse();
+    for (let i in scale) {
+      newPallet.colors[levels[i]].push({
+        name: `${color.name} ${levels[i]}`,
+        id: color.name.toLowerCase().replace(/ /g, '-'),
+        hex: scale[i],
+        rgb: chroma(scale[i]).css(),
+        rgba: chroma(scale[i])
+          .css()
+          .replace('rgb', 'rgba')
+          .replace(')', ',1.0')
+      });
+    }
+  }
+  return newPallet;
+}
+
+function getRange(hexColor) {
+  const end = '#fff';
+  return [
+    chroma(hexColor)
+      .darken(1.4)
+      .hex(),
+    hexColor,
+    end
+  ];
+}
+
+function generateScale(hexColor, numberOfColors) {
+  return chroma
+    .scale(getRange(hexColor))
+    .mode('lab')
+    .colors(numberOfColors);
+}
+
+export { generatePallet };
