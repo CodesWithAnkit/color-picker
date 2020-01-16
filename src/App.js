d3b12d3edd63bea5afc7be598adc2148c0fcf623
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-01-16 16:17:19 +0530

    Pallet routes with react-router-dom using ID

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Pallet from './Pallet';
 import ColorPaller from './seedColors';
 import { generatePallet } from './ColorHelpers';
 import './App.css';
 
-function App() {
-  console.log(generatePallet(ColorPaller[2]));
+class App extends Component {
+  findPallet = id => {
+    return ColorPaller.find(pallet => pallet.id === id);
+  };
 
-  return (
-    <div className="App">
-      <Pallet palette={generatePallet(ColorPaller[2])} />
-    </div>
-  );
+  render() {
+    console.log(generatePallet(ColorPaller[2]));
+
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>Hello world</h1>} />
+        <Route
+          path="/pallet/:id"
+          render={route => (
+            <Pallet
+              palette={generatePallet(this.findPallet(route.match.params.id))}
+            />
+          )}
+        />
+      </Switch>
+    );
+  }
 }
 
 export default App;
