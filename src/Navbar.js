353df456bfce6c0c7951798f787184571ed954db
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-01-16 15:17:33 +0530

    Adding slidebar from materialUi

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import Slider from 'rc-slider';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
 class Navbar extends Component {
   state = {
-    format: 'hex'
+    format: 'hex',
+    open: false
   };
 
   handleChange = e => {
-    this.setState({ format: e.target.value });
+    this.setState({ format: e.target.value, open: true });
     this.props.handleChange(e.target.value);
   };
+
+  snackbarClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
     const { level, changeLevel, handleChange } = this.props;
     const { format } = this.state;
@@ -41,6 +50,30 @@ class Navbar extends Component {
             <MenuItem value="rgba">RGBA - rgba(255,255,255,0.4)</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          open={this.state.open}
+          autoHideDuration={3000}
+          message={
+            <span id="message-id">
+              Format Changed to {format.toUpperCase()}
+            </span>
+          }
+          ContentProps={{
+            'aria-describedby': 'message-id'
+          }}
+          onClose={this.snackbarClose}
+          action={[
+            <IconButton
+              onClick={this.snackbarClose}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </header>
     );
   }
